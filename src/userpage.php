dd5c6e4bb3b1d4b7d51fc5b746a38e9c4aacdaec
matt200500 <matteocusanelli@gmail.com>
2024-11-01 16:35:30 -0600

    added new user functionality

--- src/userpage.php
+++ src/userpage.php
@@ -32,8 +32,57 @@ if ($_SESSION['username'] === $admin){
             echo "<script>window.location.href='index.php';</script>";
             exit();
         }
+
+        $database = 'db'; // database service from docker compose
+        $databasename = getenv('DATABASE'); // database name
+        $user = getenv('USER'); // user name    
+        $rootpassword = getenv('ROOT_PASSWORD'); // root password
+        $connection = new mysqli($database, $user, $rootpassword, $databasename); // create connection to database
+
+        // Check connection to the database
+        if ($connection->connect_error) {
+            die("Connection failed: " . $connection->connect_error);
+        }
+
+        if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['password_change'])) {
+            if (empty($_POST['new_password'])){ // if there is no password
+            }else{ // if there is a pasword
+                $new_password = $_POST['new_password'];
+                echo "Password entered: " . htmlspecialchars($new_password); // Debugging output
+                $hashed_password = password_hash($_POST['new_password'], PASSWORD_BCRYPT); // has new password
+                $sql_statement = $connection->prepare("UPDATE users SET password = ? WHERE username = ?");
+                $sql_statement->bind_param("ss", $hashed_password, $_SESSION['username']);
+                
+                if ($sql_statement->execute()){
+                    echo "<script>alert('Successfully changed password to ');</script>";
+                }
+                else{
+                    echo "<script>alert('could not change password');<script>";
+                }
+                
+                $sql_statement->close();
+            }
+        }
+
+        $connection->close();
         ?>
 
+        <div class="change_password">
+            <h2 id="password_header"> Change Password</h2>
+            <p id="password_text">Type a new password below to change it</p>
+            <form class="form" method="post" action="">
+                <input
+                    id="signup_password"
+                    type="password"
+                    name="new_password"
+                    placeholder="password"
+                    required
+                />
+                <div class="button-container">
+                    <button class="button" type="submit" id="password_button" name="password_change">Change Password</button>
+                </div>
+            </form>
+        </div>
         <form method="post" action="">
             <button type="submit" name="logout" class="button">Logout</button>
         </form>
